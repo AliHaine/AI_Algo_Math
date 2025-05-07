908346020f5d9c14c74c42ff63410678d72c6f32
AliHaine <alitr57120@gmail.com>
2025-05-08 00:59:08 +0200

    Add the dataset (provide by subject). Update predictor

---
+++ ft_linear_regression/trainer.py
@@ -0,0 +1,44 @@
+import csv
+
+rows = []
+
+m = 0
+theta0 = 0.0
+theta1 = 0.0
+learning_rate = 0.001
+iterations = 1000
+
+try:
+    with open('./data.csv') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            convert_rows = [int(value) for value in row]
+            rows.append(convert_rows)
+        max_km = max(row[0] for row in rows)
+        normalized_rows = [(km / max_km, price) for km, price in rows]
+        rows = normalized_rows
+        m = len(rows)
+except FileNotFoundError:
+    print("File not found")
+    exit(1)
+
+print(rows)
+
+def prediction(milleage):
+    return theta0 + theta1 * milleage
+
+for i in range(iterations):
+    error_t0 = 0
+    error_t1 = 0
+
+    for row in rows:
+        predict_error = prediction(row[0]) - row[1]
+        error_t0 += predict_error
+        error_t1 += predict_error * row[0]
+
+    theta0 -= learning_rate * (1 / m) * error_t0
+    theta1 -= learning_rate * (1 / m) * error_t1
+
+
+print(theta0, theta1)
