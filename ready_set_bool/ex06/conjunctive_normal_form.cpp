8dc8ff89c95e88f8f6f9cbd30cc4b8cd4957e55d
AliHaine <alitr57120@gmail.com>
2025-05-29 19:11:11 +0200

    ready set bool, update ex06

--- ready_set_bool/ex06/conjunctive_normal_form.cpp
+++ ready_set_bool/ex06/conjunctive_normal_form.cpp
@@ -3,8 +3,24 @@
 Node* toCNF(Node* node) {
 	if (node == nullptr) return nullptr;
 
+	if (isVariable(node->value))
+		return node;
 
-	return node;
+	if (node->value == '&')
+		return createNodeWithValues('&', toCNF(node->left), toCNF(node->right));
+
+	if (node->value == '|') {
+		Node* A_cnf = toCNF(node->left);
+		Node* B_cnf = toCNF(node->right);
+
+		if (A_cnf->value == '&' || B_cnf->value == '&') {
+			//todo
+			return distribute_OR_over_AND(A_cnf, B_cnf);
+		}
+		else
+			return createNodeWithValues('|', A_cnf, B_cnf);
+	}
+	return nullptr;
 }
 
 std::string conjunctive_normal_form(std::string formula) {
